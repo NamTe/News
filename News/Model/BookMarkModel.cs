353e098bd089ba0ba4cadfd3ffb9b868cfdcfa70
NamTe <tintac_tk3@yahoo.com>
2015-03-21 02:35:18 +0700

    The Final commmit

---
+++ News/Model/BookMarkModel.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO.IsolatedStorage;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace News.Model {
+    public class BookMarkModel {
+        public bool IsLoadData = false;
+        public ObservableCollection<NewsItem> bookMarkList;
+        public BookMarkModel() {
+
+        }
+
+        public void LoadData() {
+            try {
+                String SavedList;
+                if (IsolatedStorageSettings.ApplicationSettings.TryGetValue(Helper.ListSaveKey, out SavedList)) {
+                    bookMarkList = JsonConvert.DeserializeObject<ObservableCollection<NewsItem>>(SavedList);
+                }
+                else {
+                    bookMarkList = new ObservableCollection<NewsItem>();
+                }
+                IsLoadData = true;
+            }
+            catch {
+
+            }
+
+        }
+    }
+}
